d509695ba9cf7eb681ea4240b6e72657c4ce0678
arvinddchoudhary <arvindchoudhary0809@gmail.com>
2025-02-24 15:22:06 +0530

    s3

--- school/models.py
+++ school/models.py
@@ -1,39 +1,35 @@
 from django.db import models # type: ignore
 
-# Create your models here.
-
 class Course(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration_days = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)  # Store S3 image URL
 
-    def __str__(self):
+    def _str_(self):
         return self.name
 
 class FaqItem(models.Model):
     question = models.CharField(max_length=500)
     answer = models.TextField()
 
-    def __str__(self):
+    def _str_(self):
         return self.question
-    
-class Home(models.Model):
 
+class Home(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='home_images/')
+    image_url = models.URLField(blank=True, null=True)  # Store S3 image URL
 
-    def __str__(self):
+    def _str_(self):
         return self.title
-    
 
 class Instructors(models.Model):
     name = models.CharField(max_length=255)
-    experience = models.PositiveIntegerField()  # years of experience
+    experience = models.PositiveIntegerField()
     specialization = models.CharField(max_length=255)
-    description = models.TextField()  # A field for a detailed description
-    image = models.ImageField(upload_to='instructors/')
+    description = models.TextField()
+    image_url = models.URLField(blank=True, null=True)  # Store S3 image URL
 
-    def __str__(self):
+    def _str_(self):
         return self.name
